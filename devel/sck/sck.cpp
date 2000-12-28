8089d24e5effa93a365932b72f31ee73a17e59ea
epeios-q37 <epeios-q37@users.noreply.github.com>
2000-12-28 18:17:11 +0000

    Real initial upload.

--- devel/sck/sck.cpp
+++ devel/sck/sck.cpp
@@ -54,12 +54,172 @@ public:
 				  /*******************************************/
 /*$BEGIN$*/
 
-namespace sck {
+using namespace sck;
+
+#ifdef CPE__MS
+bool sck::Ready_ = false;
+#elif defined( SCK__UNIX_LIKE )
+bool sck::Ready_ = true;
+#else
+#	error "Unknow compiler enviroment"
+#endif
+
+amount__ sck::Read(
+	socket__ Socket,
+	amount__ Amount,
+	void *Buffer,
+	duration__ TimeOut )
+{
+	fd_set fds;
+	int Result;
+
+	FD_ZERO( &fds );
+	FD_SET( Socket, &fds );
+
+	if ( TimeOut == SCK_INFINITE )
+		Result = select( Socket + 1, &fds, NULL, NULL, NULL );
+	else
+	{
+		timeval TV;
+
+		TV.tv_sec = TimeOut;
+		TV.tv_usec = 0;
+
+		Result = select( Socket + 1, &fds, NULL, NULL, &TV );
+	}
+
+	if ( Result == 1 )
+	{
+		Result = recv( Socket, (sck__cast)Buffer, Amount, 0 );
+
+		if ( Result == SCK_SOCKET_ERROR )
+		{
+			Result = SCK_DISCONNECTED;
+
+			if ( Error() != SCK_ECONNRESET )
+				ERRd();
+		}
+		else if ( !Result && Amount )
+			Result = SCK_DISCONNECTED;
+	}
+	else if ( Result == SCK_SOCKET_ERROR )
+		ERRd();
+	else if ( Result != 0 )
+		ERRs();
+
+	return Result;
 }
+
+amount__ sck::Write(
+	socket__ Socket,
+	const void *Buffer,
+	amount__ Amount,
+	duration__ TimeOut )
+{
+	fd_set fds;
+	int Result;
+
+	FD_ZERO( &fds );
+	FD_SET( Socket, &fds );
+
+	if ( TimeOut == SCK_INFINITE )
+		Result = select( Socket + 1, NULL, &fds, NULL, NULL );
+	else
+	{
+		timeval TV;
+
+		TV.tv_sec = TimeOut;
+		TV.tv_usec = 0;
+
+		Result = select( Socket + 1, NULL, &fds, NULL, &TV );
+	}
+
+	if ( Result == 1 )
+	{
+		Result = send( Socket, (const sck__cast)Buffer, Amount, 0 );
+
+		if ( Result == SCK_SOCKET_ERROR )
+		{
+			Result = SCK_DISCONNECTED;
+
+			if ( Error() != SCK_ECONNRESET )
+				ERRd();
+		}
+		else if ( !Result && Amount )	// I assume that, because the send is call immediatly after the select, this can not be happen.
+			ERRc();
+	}
+	else if ( Result == SCK_SOCKET_ERROR )
+		ERRd();
+	else if ( Result != 0 )
+		ERRs();
+
+	return Result;
+}
+
+
+amount__ sck::Get_(
+	socket__ Socket,
+	amount__ Minimum,
+	data__ *Buffer,
+	amount__ Wanted,
+	duration__ TimeOut )
+{
+	amount__ Amount = 0;
+
+	if ( Minimum != 0 )
+	{
+		amount__ Result;
+		
+		while( Minimum > Amount ) {
+			if ( ( Result = Read( Socket, Wanted - Amount, Buffer + Amount, TimeOut ) ) == 0 )
+				ERRd();
+			Amount += Result;
+		}
+	}
+	else
+		Amount = Read( Socket, Wanted, Buffer, 0 );
+
+	if ( Amount == SCK_DISCONNECTED )
+		ERRd();
+
+	return Amount;
+}
+
+
+amount__ sck::Put_(
+	socket__ Socket,
+	const data__ *Buffer,
+	amount__ Wanted,
+	amount__ Minimum,
+	bool,
+	duration__ TimeOut )
+{
+	amount__ Amount = 0;
+
+	if ( Minimum != 0 )
+	{
+		amount__ Result;
+		
+		while( Minimum > Amount ) {
+			if ( ( Result = Write( Socket, Buffer + Amount, Wanted - Amount, TimeOut ) ) == 0 )
+				ERRd();
+			Amount += Result;
+		}
+	}
+	else
+		Amount = Write( Socket, Buffer, Wanted, 0 );
+
+	if ( Amount == SCK_DISCONNECTED )
+		ERRd();
+
+	return Amount;
+}
+
+
+
 
 /* Although in theory this class is inaccessible to the different modules,
 it is necessary to personalize it, or certain compiler would not work properly */
-
 class sckpersonnalization
 : public scktutor
 {
