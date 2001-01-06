de4d9efdd9c0ccf041e5b07d23eb968a12ead092
epeios-q37 <epeios-q37@users.noreply.github.com>
2001-01-06 20:37:45 +0000

    Initial upload.

---
+++ devel/idxque/idxque.h
@@ -0,0 +1,142 @@
+/*
+  Header for the 'idxque' library by Claude L. Simon (simon@epeios.org)
+  Copyright (C) 2000,2001 Claude L. SIMON (simon@epeios.org) 
+
+  This file is part of the Epeios (http://www.epeios.org/) project.
+  
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, go to http://www.fsf.org/
+  or write to the:
+  
+                        Free Software Foundation, Inc.,
+           59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+//	$Id$
+
+#ifndef IDXQUE__INC
+#define IDXQUE__INC
+
+#define IDXQUE_NAME		"IDXQUE"
+
+#define	IDXQUE_VERSION	"$Revision$"	
+
+#define IDXQUE_OWNER		"the Epeios project (http://www.epeios.org/)"
+
+#include "ttr.h"
+
+extern class ttr_tutor &IDXQUETutor;
+
+#if defined( XXX_DBG ) && !defined( IDXQUE_NODBG )
+#define IDXQUE_DBG 
+#endif
+
+/* Begin of automatic documentation generation part. */
+
+//V $Revision$
+//C Claude L. SIMON (simon@epeios.org)
+//R $Date$
+
+/* End of automatic documentation generation part. */
+
+/******************************************************************************/
+				  /* do not modify anything above this limit */
+				  /*			  unless specified			 */
+				  /*******************************************/
+/*$BEGIN$*/
+
+#include "err.h"
+#include "txf.h"
+#include "que.h"
+#include "stk.h"
+
+//d The end of the queue.
+#define IDXQUE_END	NONE
+
+struct idxque
+{
+	enum direction
+	{
+		dAscending = false,
+		dDescending = true
+	};
+};
+
+//c Queue-based index, fast browsing, but slow sorting.
+class idxque_queue_index_
+: public MQUEUE_
+{
+public:
+	struct s
+	: public MQUEUE_::s
+	{};
+// fonctions
+	idxque_queue_index_( s &S )
+	: MQUEUE_( S )
+	{}
+	void reset( bool P = true )
+	{
+		MQUEUE_::reset( P );
+	}
+	void plug( mmm_multimemory_ &MM )
+	{
+		MQUEUE_::plug( MM );
+	}
+	void plug( MEMORY_DRIVER_ &MD )
+	{
+		MQUEUE_::plug( MD );
+	}
+	idxque_queue_index_ &operator =( const idxque_queue_index_ &T )
+	{
+		MQUEUE_::operator =( T );
+
+		return *this;
+	}
+	//f Initialization.
+	void Init( void )
+	{
+		MQUEUE_::Init();
+	}
+	/*f Dump to the stack 'Stack' the queue beginning at 'Begin' and in the 'Direction ' direction.. */
+	void Dump(
+		stk_stack_<POSITION__> &Stack,
+		POSITION__ Begin,
+		idxque::direction Direction );
+	//f Item becomes node next to 'Node'.
+	void BecomeNext(
+		POSITION__ Item,
+		POSITION__ Node )
+	{
+		MQUEUE_::InsertItemAfterNode( Item, Node );
+	}
+	//f Item becomes node previous to 'Node'.
+	void BecomePrevious(
+		POSITION__ Item,
+		POSITION__ Node )
+	{
+		MQUEUE_::InsertItemBeforeNode( Item, Node );
+	}
+};
+
+
+AUTO( idxque_queue_index )
+
+
+/*$END$*/
+				  /********************************************/
+				  /* do not modify anything belove this limit */
+				  /*			  unless specified		   	  */
+/******************************************************************************/
+
+#endif
