9863b0569491c76f58e58399d552f8ed007ddae1
epeios-q37 <epeios-q37@users.noreply.github.com>
2001-01-08 09:46:29 +0000

    Initial upload.

---
+++ devel/txmtbl/txmtbl.cpp
@@ -0,0 +1,557 @@
+/*
+  'txmtbl' library by Claude L. Simon (simon@epeios.org)
+  Requires the 'txmtbl' header file ('txmtbl.h').
+  Copyright (C) 2000,2001 Claude L. SIMON (simon@epeios.org).
+
+  This file is part of the Epeios (http://www.epeios.org/) project.
+  
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, go to http://www.fsf.org/
+  or write to the:
+  
+                        Free Software Foundation, Inc.,
+           59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+//	$Id$
+
+#define TXMTBL__COMPILATION
+
+#include "txmtbl.h"
+
+class txmtbltutor
+: public ttr_tutor
+{
+public:
+	txmtbltutor( void )
+	: ttr_tutor( TXMTBL_NAME )
+	{
+#ifdef TXMTBL_DBG
+		Version = TXMTBL_VERSION "\b\bD $";
+#else
+		Version = TXMTBL_VERSION;
+#endif
+		Owner = TXMTBL_OWNER;
+		Date = "$Date$";
+	}
+	virtual ~txmtbltutor( void ){}
+};
+
+/******************************************************************************/
+				  /* do not modify anything above this limit */
+				  /*			  unless specified			 */
+				  /*******************************************/
+/*$BEGIN$*/
+
+static inline bso__bool IsNotEndOfCell_(
+	xtf::extended_text_iflow___ &Flow,
+	txmtbl__separator Separator,
+	bso__bool &EOX,
+	bso__char &C )
+{
+	return !( EOX = Flow.EOX() ) && ( ( C = Flow.Get() ) != Separator ) && ( C != '\n' ) && ( C != '\r' );
+}
+
+static inline txmtbl::delimiter GetDelimiter_( 
+	xtf::extended_text_iflow___ &Flow,
+	txmtbl__separator Separator,
+	bso__bool EOX,
+	bso__char C )
+{
+	if ( EOX )
+		return txmtbl::dEOF;
+	else if ( C == '\n' )
+	{
+		if ( !Flow.EOX() && ( Flow.View() == '\r' ) )
+			Flow.Get();
+
+		return txmtbl::dEOL;
+	}
+	else if ( C == '\r' )
+	{
+		if ( !Flow.EOX() && ( Flow.View() == '\n' ) )
+			Flow.Get();
+
+		return txmtbl::dEOL;
+	}
+	else if ( C == Separator )
+		return txmtbl::dSeparator;
+	else
+	{
+		ERRc();
+		return txmtbl::dUnknow;
+	}
+}
+
+txmtbl::delimiter TXMTBLGetCell(
+	xtf::extended_text_iflow___ &Flow,
+	txmtbl_cell_ &Cell,
+	txmtbl__separator Separator )
+{
+	bso__char C = 0;
+	bso__bool EOX = false;
+
+	Cell.Init();
+
+	Cell.Location( Flow.Column() );
+
+	while( IsNotEndOfCell_( Flow, Separator,EOX,C ) ) 
+		Cell.Add( C );
+	
+	return GetDelimiter_( Flow, Separator, EOX, C );
+}
+
+txmtbl::delimiter TXMTBLSkipCell(
+	xtf::extended_text_iflow___ &Flow,
+	txmtbl__separator Separator )
+{
+	bso__char C = 0;
+	bso__bool EOX = false;
+
+	while( IsNotEndOfCell_( Flow, Separator,EOX,C ) );
+
+	return GetDelimiter_( Flow, Separator, EOX, C );
+}
+
+bso__bool TXMTBLGetLine(
+	xtf::extended_text_iflow___ &Flow,
+	txmtbl_line_ &Line,
+	txmtbl__separator Separator )
+{
+ERRProlog
+	txmtbl_cell Cell;
+	bso__bool Loop;
+	txmtbl__location Location;
+ERRBegin
+	Cell.Init();
+
+	do {
+		Location = Flow.Line();
+
+		Loop = ( TXMTBLGetCell( Flow, Cell, Separator ) == txmtbl::dSeparator ) && !Flow.EOX();
+
+		if ( Loop || Cell.Amount() || Line.Amount() )
+			Line.Add( Cell, Location );
+
+	}while ( Loop );
+ERRErr
+ERREnd
+ERREpilog
+
+	return !Flow.EOX();
+}
+
+txmtbl__amount txmtbl_line_::DeleteEmptyCells( void )
+{
+	CMITEM( txmtbl_cell_ ) Cell;
+	POSITION__ Temp, Current = First();
+	txmtbl__amount Amount = 0;
+
+	Cell.Init( *this );
+
+	while( Current != NONE )
+	{
+		Temp = Current;
+		Current = Next( Current );
+
+		if ( !Cell( Temp ).Amount() )
+		{
+			DeleteCell_( Temp );
+			Amount++;
+		}
+	}
+
+	return Amount;
+}
+
+POSITION__ txmtbl_line_::FirstNonEmptyCell( void ) const
+{
+	CMITEM( txmtbl_cell_ ) Cell;
+	POSITION__ Current = First();
+
+	Cell.Init( *this );
+
+	while( ( Current != NONE ) && !Cell( Current ).Amount() )
+		Current = Next( Current );
+
+	return Current;
+}
+
+POSITION__ txmtbl_line_::LastNonEmptyCell( void ) const
+{
+	CMITEM( txmtbl_cell_ ) Cell;
+	POSITION__ Current = Last();
+
+	Cell.Init( *this );
+
+	while( ( Current != NONE ) && !Cell( Current ).Amount() )
+		Current = Previous( Current );
+
+	return Current;
+}
+
+txmtbl__amount txmtbl_line_::DeleteHeadingEmptyCells( void )
+{
+	POSITION__ Current = FirstNonEmptyCell(), Temp;
+	txmtbl__amount Amount = 0;
+
+	if ( Current != NONE )
+	{
+		Current = Previous( Current );
+
+		while( Current != NONE )
+		{
+			Temp = Current;
+
+			Current = Previous( Current );
+
+			DeleteCell_( Temp );
+
+			Amount++;
+		}
+	}
+	else if ( this->Amount() )
+		DeleteAllCells();
+
+	return Amount;
+}
+
+txmtbl__amount txmtbl_line_::DeleteTailingEmptyCells( void )
+{
+	POSITION__ Current = LastNonEmptyCell(), Temp;
+	txmtbl__amount Amount = 0;
+
+	if ( Current != NONE )
+	{
+		Current = Next( Current );
+
+		while( Current != NONE )
+		{
+			Temp = Current;
+
+			Current = Next( Current );
+
+			DeleteCell_( Temp );
+
+			Amount++;
+		}
+	}
+	else if ( this->Amount() )
+		DeleteAllCells();
+
+	return Amount;
+}
+
+txmtbl__amount txmtbl_line_::DeleteCentralEmptyCells( void )
+{
+	CMITEM( txmtbl_cell_ ) Cell;
+	POSITION__
+		Current = FirstNonEmptyCell(),
+		Last = LastNonEmptyCell(),
+		Temp;
+	txmtbl__amount Amount = 0;
+
+	Cell.Init( *this );
+
+	if ( Current != Last )
+	{
+		Current = Next( Current );
+
+		while( Current != Last )
+		{
+			Temp = Current;
+
+			Current = Next( Current );
+
+			if ( !Cell( Temp ).Amount() )
+			{
+				DeleteCell_( Temp );
+				Amount++;
+			}
+		}
+	}
+	else if ( this->Amount() && ( Current == NONE ) )
+		DeleteAllCells();
+
+	return Amount;
+}
+
+txmtbl__amount txmtbl_line_::DeleteCellsAt( POSITION__ Position )
+{
+	POSITION__ Temp;
+	txmtbl__amount Amount = 0;
+
+	while( Position != NONE )
+	{
+		Temp = Position;
+
+		Position = Next( Position );
+
+		DeleteCell_( Temp );
+
+		Amount++;
+	}
+
+	return Amount;
+}
+
+static inline bool IsCommentary_(
+	const txmtbl_cell_ &Cell,
+	bso__char Marker )
+{
+	return Cell.Amount() && ( Cell( 0 ) == Marker );
+}
+
+txmtbl__amount txmtbl_line_::DeleteCommentary( bso__char Marker )
+{
+	POSITION__ Position = First();
+	CMITEM( txmtbl_cell_ ) Cell;
+
+	Cell.Init( *this );
+
+	while( ( Position != NONE ) && !IsCommentary_( Cell( Position ), Marker ) )
+		Position = Next( Position );
+
+	if ( Position != NONE )
+		return DeleteCellsAt( Position );
+	else
+		return 0;
+}
+
+txf::text_oflow___ &operator <<(
+	txf::text_oflow___ &Flow,
+	const txmtbl_line_ &Line )
+{
+	POSITION__ Current;
+	CMITEM( txmtbl_cell_ ) Cell;
+
+	Cell.Init( Line );
+
+	Current = Line.First();
+
+	if ( Current != NONE )
+	{
+		Flow << Cell( Current );
+		Current = Line.Next( Current );
+	}
+
+	while( Current != NONE )
+	{
+		Flow << txf::tab << Cell( Current );
+		Current = Line.Next( Current );
+	}
+
+	return Flow;
+}
+
+bso__bool TXMTBLGetFirstNonEmptyLine(
+	xtf::extended_text_iflow___ &Flow,
+	txmtbl_line_ &Line,
+	txmtbl__separator Separator )
+{
+	if ( Flow.EOX() )
+		return false;
+	else
+	{
+		do
+		{
+			Line.Init();
+			TXMTBLGetLine( Flow, Line );
+
+			Line.DeleteEmptyCells();
+
+		} while( !Line.Amount() && !Flow.EOX() );
+
+		return Line.Amount() != 0;
+	}
+}
+
+
+void TXMTBLGetTable(
+	xtf::extended_text_iflow___ &Flow,
+	txmtbl_table_ &Table,
+	txmtbl__separator Separator )
+{
+ERRProlog
+	txmtbl_line Line;
+ERRBegin
+	while( !Flow.EOX() )
+	{
+		Line.Init();
+
+		Line.Location( Flow.Line() );
+
+		TXMTBLGetLine( Flow, Line, Separator );
+
+		Table.AddLine( Line );
+	}
+ERRErr
+ERREnd
+ERREpilog
+}
+
+void txmtbl_table_::DeleteEmptyCells( void )
+{
+	POSITION__ Current = First();
+
+	while( Current != NONE )
+	{
+		CONTAINER_( txmtbl_line_ )::operator ()( Current ).DeleteEmptyCells();
+
+		Current = Next( Current );
+	}
+
+	CONTAINER_( txmtbl_line_ )::Sync();
+}
+
+void txmtbl_table_::DeleteHeadingEmptyCells( void )
+{
+	POSITION__ Current = First();
+
+	while( Current != NONE )
+	{
+		CONTAINER_( txmtbl_line_ )::operator ()( Current ).DeleteHeadingEmptyCells();
+
+		Current = Next( Current );
+	}
+
+	CONTAINER_( txmtbl_line_ )::Sync();
+}
+
+void txmtbl_table_::DeleteTailingEmptyCells( void )
+{
+	POSITION__ Current = First();
+
+	while( Current != NONE )
+	{
+		CONTAINER_( txmtbl_line_ )::operator ()( Current ).DeleteTailingEmptyCells();
+
+		Current = Next( Current );
+	}
+
+	CONTAINER_( txmtbl_line_ )::Sync();
+}
+
+void txmtbl_table_::DeleteCentralEmptyCells( void )
+{
+	POSITION__ Current = First();
+
+	while( Current != NONE )
+	{
+		CONTAINER_( txmtbl_line_ )::operator()( Current ).DeleteCentralEmptyCells();
+
+		Current = Next( Current );
+	}
+
+	CONTAINER_( txmtbl_line_ )::Sync();
+}
+
+void txmtbl_table_::DeleteCommentaries( bso__char Marker )
+{
+	POSITION__ Current = First();
+
+	while( Current != NONE )
+	{
+		CONTAINER_( txmtbl_line_ )::operator()( Current ).DeleteCommentary( Marker );
+
+		Current = Next( Current );
+	}
+
+	CONTAINER_( txmtbl_line_ )::Sync();
+}
+
+txmtbl__amount txmtbl_table_::DeleteEmptyLines( void )
+{
+	CITEM( txmtbl_line_ ) Line;
+	POSITION__ Current = First(), Temp;
+	txmtbl__amount Amount = 0;
+
+	Line.Init( *this );
+
+	while( Current != NONE )
+	{
+		Temp = Current;
+
+		Current = Next( Current );
+
+		if ( !Line( Temp).Amount() )
+		{
+			DeleteLine_( Temp );
+			Amount++;
+		}
+	}
+
+	return Amount;
+
+}
+
+txf::text_oflow___ &operator <<(
+	txf::text_oflow___ &Flow,
+	const txmtbl_table_ &Table )
+{
+	CITEM( txmtbl_line_ ) Line;
+	POSITION__ Current;
+
+	Line.Init( Table );
+
+	Current = Table.First();
+
+	if ( Current != NONE )
+	{
+		Flow << Line( Current );
+		Current = Table.Next( Current );
+	}
+
+	while( Current != NONE )
+	{
+		Flow << txf::nl << Line( Current );
+		Current = Table.Next( Current );
+	}
+
+	return Flow;
+}
+
+
+
+/* Although in theory this class is inaccessible to the different modules,
+it is necessary to personalize it, or certain compiler would not work properly */
+
+class txmtblpersonnalization
+: public txmtbltutor
+{
+public:
+	txmtblpersonnalization( void )
+	{
+		/* place here the actions concerning this library
+		to be realized at the launching of the application  */
+	}
+	~txmtblpersonnalization( void )
+	{
+		/* place here the actions concerning this library
+		to be realized at the ending of the application  */
+	}
+};
+
+
+/*$END$*/
+				  /********************************************/
+				  /* do not modify anything belove this limit */
+				  /*			  unless specified		   	  */
+/******************************************************************************/
+
+// 'static' by GNU C++.
+
+static txmtblpersonnalization Tutor;
+
+ttr_tutor &TXMTBLTutor = Tutor;
---
+++ devel/txmtbl/txmtbl.h
@@ -0,0 +1,409 @@
+/*
+  Header for the 'txmtbl' library by Claude L. Simon (simon@epeios.org)
+  Copyright (C) 2000,2001 Claude L. SIMON (simon@epeios.org) 
+
+  This file is part of the Epeios (http://www.epeios.org/) project.
+  
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, go to http://www.fsf.org/
+  or write to the:
+  
+                        Free Software Foundation, Inc.,
+           59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+//	$Id$
+
+#ifndef TXMTBL__INC
+#define TXMTBL__INC
+
+#define TXMTBL_NAME		"TXMTBL"
+
+#define	TXMTBL_VERSION	"$Revision$"	
+
+#define TXMTBL_OWNER		"the Epeios project (http://www.epeios.org/)"
+
+#include "ttr.h"
+
+extern class ttr_tutor &TXMTBLTutor;
+
+#if defined( XXX_DBG ) && !defined( TXMTBL_NODBG )
+#define TXMTBL_DBG 
+#endif
+
+/* Begin of automatic documentation generation part. */
+
+//V $Revision$
+//C Claude L. SIMON (simon@epeios.org)
+//R $Date$
+
+/* End of automatic documentation generation part. */
+
+/******************************************************************************/
+				  /* do not modify anything above this limit */
+				  /*			  unless specified			 */
+				  /*******************************************/
+/*$BEGIN$*/
+
+//D TeXt Manager TaBLe. Table like text flow manager.
+
+#include "err.h"
+#include "flw.h"
+#include "str.h"
+#include "lst.h"
+#include "ctn.h"
+#include "xtf.h"
+
+struct txmtbl
+{
+	//e The different delimiter.
+	enum delimiter
+	{
+		//i End of line.
+		dEOL,
+		//i Separator
+		dSeparator,
+		//i End of flow
+		dEOF,
+		//i Unknow delimiter.
+		dUnknow
+	};
+};
+
+//t An amount of cell or of line.
+typedef SIZE__	txmtbl__amount;
+
+//d The default cell separator.
+#define TXMTBL_DEFAULT_CELL_SEPARATOR	'\t'
+
+//t A location (line or column).
+typedef xtf::location	txmtbl__location;
+
+//t A separator.
+typedef bso__char	txmtbl__separator;
+
+//c A cell.
+class txmtbl_cell_
+: public str_string_
+{
+public:
+	struct s
+	: public str_string_::s
+	{
+		txmtbl__location Location;
+	} &S_;
+	txmtbl_cell_( s &S )
+	: S_( S ),
+	  str_string_( S )
+	{}
+	void reset( bso__bool P = true )
+	{
+		str_string_::reset( P );
+		S_.Location = 0;
+	}
+	void plug( mmm_multimemory_ &M )
+	{
+		str_string_::plug( M );
+	}
+	void plug( MEMORY_DRIVER_ &MD )
+	{
+		str_string_::plug( MD );
+	}
+	txmtbl_cell_ &operator =( const txmtbl_cell_ &C )
+	{
+		str_string_::operator =( C );
+
+		S_.Location = C.S_.Location;
+
+		return *this;
+	}
+	//f Initialization.
+	void Init( void )
+	{
+		str_string_::Init();
+
+		S_.Location = 0;
+	}
+	//f 'Location' becomes the location.
+	void Location( txmtbl__location Location )
+	{
+		S_.Location = Location;
+	}
+	//f Return location of the cell.
+	txmtbl__location Location( void ) const
+	{
+		return S_.Location;
+	}
+};
+
+AUTO( txmtbl_cell )
+
+/*f Put in 'Cell' the next cell in flow 'Flow'. 'Separator' delimits the celles.
+Return the delimiter of the cell. */
+txmtbl::delimiter TXMTBLGetCell(
+	xtf::extended_text_iflow___ &Flow,
+	txmtbl_cell_ &Cell,
+	txmtbl__separator Separator = TXMTBL_DEFAULT_CELL_SEPARATOR );
+
+/*f Skip next cell in flow 'Flow'. 'Separator' delimits the cells.
+Return the delimiter of the cell. */
+txmtbl::delimiter TXMTBLSkipCell(
+	xtf::extended_text_iflow___ &Flow,
+	txmtbl__separator Separator = TXMTBL_DEFAULT_CELL_SEPARATOR );
+
+
+//c A line of cells
+class txmtbl_line_
+: public LIST_,
+  public MCONTAINER_( txmtbl_cell_ )
+{
+private:
+	// Supprimer la cellule à la position 'Position'.
+	void DeleteCell_( POSITION__ Position )
+	{
+		MCONTAINER_( txmtbl_cell_ )::operator()( Position ).Allocate( 0 );
+		MCONTAINER_( txmtbl_cell_ )::Sync();
+
+		LIST_::Remove( Position );
+	}
+protected:
+	virtual void LSTAllocate( SIZE__ Size )
+	{
+		MCONTAINER_( txmtbl_cell_ )::Allocate( Size );
+	}
+public:
+	struct s
+	: public ::LIST_::s,
+	  public MCONTAINER_( txmtbl_cell_ )::s
+	{
+		txmtbl__location Location;
+	} &S_;
+	txmtbl_line_( s &S )
+	: S_( S ),
+	  LIST_( S ),
+	  MCONTAINER_( txmtbl_cell_ )( S )
+	{}
+	void reset( bool P = true )
+	{
+		LIST_::reset( P );
+		MCONTAINER_( txmtbl_cell_ )::reset( P );
+
+		S_.Location = 0;
+	}
+	void plug( mmm_multimemory_ &M )
+	{
+		LIST_::plug( M );
+		MCONTAINER_( txmtbl_cell_ )::plug( M );
+	}
+	txmtbl_line_ &operator =( const txmtbl_line_ &L )
+	{
+		LIST_::operator =( L );
+		MCONTAINER_( txmtbl_cell_ )::operator =( L );
+
+		S_.Location = L.S_.Location;
+
+		return *this;
+	}
+	//f Initialization.
+	void Init( void )
+	{
+		LIST_::Init();
+		MCONTAINER_( txmtbl_cell_ )::Init();
+
+		S_.Location = 0;
+	}
+	//f Write 'Cell' and 'Location' at 'Position'.
+	void Write(
+		const txmtbl_cell_ &Cell,
+		txmtbl__location Location,
+		POSITION__ Position )
+	{
+		MCONTAINER_( txmtbl_cell_ )::Write( Cell, Position );
+		MCONTAINER_( txmtbl_cell_ )::Sync();
+
+		S_.Location = Location;
+	}
+	//f Add 'Cell' and 'Location'. Return position where added.
+	POSITION__ Add(
+		const txmtbl_cell_ &Cell,
+		txmtbl__location Location )
+	{
+		POSITION__ P = LIST_::CreateEntry();
+
+		Write( Cell, Location, P );
+
+		return P;
+	}
+	//f Return the position of the first non-empty cell.
+	POSITION__ FirstNonEmptyCell( void ) const;
+	//f Return the position of the last non-empty cell.
+	POSITION__ LastNonEmptyCell( void ) const;
+	//f Delete all empty cells. Retunr amount of cells deleted.
+	txmtbl__amount DeleteEmptyCells( void );
+	//f Delete all heading empty cells. Return amount of cells deleted.
+	txmtbl__amount DeleteHeadingEmptyCells( void );
+	//f Delete all tailing empty cells. Return amount of cells deleted.
+	txmtbl__amount DeleteTailingEmptyCells( void );
+	//f Delete all empty cells between the first and last non-empty cells. Return amount of cells deleted.
+	txmtbl__amount DeleteCentralEmptyCells( void );
+	//f Delete all cells from 'Position'. Return amount of cells deleted.
+	txmtbl__amount DeleteCellsAt( POSITION__ Position );
+	//f Delete all cells. Return amount of cells deleted.
+	txmtbl__amount DeleteAllCells( void )
+	{
+		POSITION__ P = LIST_::First();
+
+		if ( P != NONE )
+			return DeleteCellsAt( P );
+		else
+			return 0;
+	}
+	/*f Delete the cells beginnig with 'Marker' and all following cells from the same line.
+	Return amount of cell deleted.*/
+	txmtbl__amount DeleteCommentary( bso__char Marker );
+	//f 'Location' becomes the location.
+	void Location( txmtbl__location Location )
+	{
+		S_.Location = Location;
+	}
+	//f Return location of the line.
+	txmtbl__location Location( void ) const
+	{
+		return S_.Location;
+	}
+	FNLPA( LIST_:: )
+};
+
+txf::text_oflow___ &operator <<(
+	txf::text_oflow___ &Flow,
+	const txmtbl_line_ &Line );
+
+AUTO( txmtbl_line )
+
+/*f Put in 'Line' current line in 'Flow' and return true, or false if there is no
+more line. Cells are separated by 'Separator'.*/
+bso__bool TXMTBLGetLine(
+	xtf::extended_text_iflow___ &Flow,
+	txmtbl_line_ &Line,
+	txmtbl__separator Separator = TXMTBL_DEFAULT_CELL_SEPARATOR );
+
+/*f Put in 'Line' the first non-empty line in 'Flow' and return true, or false if there is no
+more line. Cells are separated by 'Separator'.*/
+bso__bool TXMTBLGetFirstNonEmptyLine(
+	xtf::extended_text_iflow___ &Flow,
+	txmtbl_line_ &Line,
+	txmtbl__separator Separator = TXMTBL_DEFAULT_CELL_SEPARATOR );
+
+
+
+//c A table.
+class txmtbl_table_
+: public LIST_,
+  public CONTAINER_( txmtbl_line_ )
+{
+private:
+	// Delete line at position 'Position'.
+	void DeleteLine_( POSITION__ Position )
+	{
+		CONTAINER_( txmtbl_line_ )::operator ()(Position).DeleteAllCells();
+		CONTAINER_( txmtbl_line_ )::Sync();
+
+		LIST_::Remove( Position );
+	}
+protected:
+	virtual void LSTAllocate( SIZE__ Size )
+	{
+		CONTAINER_( txmtbl_line_ )::Allocate( Size );
+	}
+public:
+	struct s
+	: public ::LIST_::s,
+	  public CONTAINER_( txmtbl_line_ )::s
+	{};
+	txmtbl_table_( s &S )
+	: LIST_( S ),
+	  CONTAINER_( txmtbl_line_ )( S )
+	{}
+	void reset( bool P = true )
+	{
+		LIST_::reset( P );
+		CONTAINER_( txmtbl_line_ )::reset( P );
+	}
+	void plug( mmm_multimemory_ &M )
+	{
+		LIST_::plug( M );
+		CONTAINER_( txmtbl_line_ )::plug( M );
+	}
+	txmtbl_table_ &operator =( const txmtbl_table_ &T )
+	{
+		LIST_::operator =( T );
+		CONTAINER_( txmtbl_line_ )::operator =( T );
+
+		return *this;
+	}
+	//f Initialization.
+	void Init( void )
+	{
+		LIST_::Init();
+		CONTAINER_( txmtbl_line_ )::Init();
+	}
+	//f Add 'Line'. Return position where added.
+	POSITION__ AddLine(	const txmtbl_line_ &Line )
+	{
+		POSITION__ P = LIST_::CreateEntry();
+
+		CONTAINER_( txmtbl_line_ )::Write( Line, P );
+		CONTAINER_( txmtbl_line_ )::Sync();
+
+		return P;
+	}
+	//f Delete the line at 'Position'.
+	void DeleteLine( POSITION__ Position )
+	{
+		DeleteLine_( Position );
+	}
+	//f Delete all emty lines. Return amount of lines deleted.
+	txmtbl__amount DeleteEmptyLines( void );
+	//f Delete all empty cells from all lines.
+	void DeleteEmptyCells( void );
+	//f Delete all heading cells from all the lines.
+	void DeleteHeadingEmptyCells( void );
+	//f Delete all tailing cells from all lines.
+	void DeleteTailingEmptyCells( void );
+	//f Delete all empty cells between the first and last non-empty cells from all the lines.
+	void DeleteCentralEmptyCells( void );
+	//f Delete, for each line, the cells beginning with 'Marker' and all following cells.
+	void DeleteCommentaries( bso__char Marker );
+	FNLPA( LIST_:: )
+};
+
+txf::text_oflow___ &operator <<(
+	txf::text_oflow___ &Flot,
+	const txmtbl_table_ &Table );
+
+AUTO( txmtbl_table )
+
+//f Put in 'Table' the line contained in 'Flow'. Cells are separated by 'Separator'.
+void TXMTBLGetTable(
+	xtf::extended_text_iflow___ &Flow,
+	txmtbl_table_ &Table,
+	txmtbl__separator Separator = TXMTBL_DEFAULT_CELL_SEPARATOR );
+
+/*$END$*/
+				  /********************************************/
+				  /* do not modify anything belove this limit */
+				  /*			  unless specified		   	  */
+/******************************************************************************/
+
+#endif
