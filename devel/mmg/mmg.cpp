10eb96c187359d5b3de184dcb2317c2cf597bc31
epeios-q37 <epeios-q37@users.noreply.github.com>
2001-01-11 15:33:15 +0000

    Initial upload.

---
+++ devel/mmg/mmg.cpp
@@ -0,0 +1,86 @@
+/*
+  'mmg' library by Claude L. Simon (simon@epeios.org)
+  Requires the 'mmg' header file ('mmg.h').
+  Copyright (C) 2000,2001 Claude L. SIMON (simon@epeios.org).
+
+  This file is part of the Epeios (http://www.epeios.org/) project.
+  
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, go to http://www.fsf.org/
+  or write to the:
+  
+                        Free Software Foundation, Inc.,
+           59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+//	$Id$
+
+#define MMG__COMPILATION
+
+#include "mmg.h"
+
+class mmgtutor
+: public ttr_tutor
+{
+public:
+	mmgtutor( void )
+	: ttr_tutor( MMG_NAME )
+	{
+#ifdef MMG_DBG
+		Version = MMG_VERSION "\b\bD $";
+#else
+		Version = MMG_VERSION;
+#endif
+		Owner = MMG_OWNER;
+		Date = "$Date$";
+	}
+	virtual ~mmgtutor( void ){}
+};
+
+/******************************************************************************/
+				  /* do not modify anything above this limit */
+				  /*			  unless specified			 */
+				  /*******************************************/
+/*$BEGIN$*/
+
+/* Although in theory this class is inaccessible to the different modules,
+it is necessary to personalize it, or certain compiler would not work properly */
+class mmgpersonnalization
+: public mmgtutor
+{
+public:
+	mmgpersonnalization( void )
+	{
+		/* place here the actions concerning this library
+		to be realized at the launching of the application  */
+	}
+	~mmgpersonnalization( void )
+	{
+		/* place here the actions concerning this library
+		to be realized at the ending of the application  */
+	}
+};
+
+
+/*$END$*/
+				  /********************************************/
+				  /* do not modify anything belove this limit */
+				  /*			  unless specified		   	  */
+/******************************************************************************/
+
+// 'static' by GNU C++.
+
+static mmgpersonnalization Tutor;
+
+ttr_tutor &MMGTutor = Tutor;
