b7ed192875267f10ccbc89247904ea37e4df5f29
epeios-q37 <epeios-q37@users.noreply.github.com>
2002-01-02 10:42:23 +0000

    Coding.

---
+++ stable/sma.cpp
@@ -0,0 +1,89 @@
+/*
+  'sma' library by Claude L. Simon (csimon@epeios.org)
+  Requires the 'sma' header file ('sma.h').
+  Copyright (C) 2000,2001 Claude L. SIMON (csimon@epeios.org).
+
+  This file is part of the Epeios (http://epeios.org/) project.
+  
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+ 
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, go to http://www.fsf.org/
+  or write to the:
+  
+                        Free Software Foundation, Inc.,
+           59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+//	$Id$
+
+#define SMA__COMPILATION
+
+#include "sma.h"
+
+class smatutor
+: public ttr_tutor
+{
+public:
+	smatutor( void )
+	: ttr_tutor( SMA_NAME )
+	{
+#ifdef SMA_DBG
+		Version = SMA_VERSION "\b\bD $";
+#else
+		Version = SMA_VERSION;
+#endif
+		Owner = SMA_OWNER;
+		Date = "$Date$";
+	}
+	virtual ~smatutor( void ){}
+};
+
+/******************************************************************************/
+				  /* do not modify anything above this limit */
+				  /*			  unless specified			 */
+				  /*******************************************/
+/*$BEGIN$*/
+
+using namespace sma;
+
+/* Although in theory this class is inaccessible to the different modules,
+it is necessary to personalize it, or certain compiler would not work properly */
+
+class smapersonnalization
+: public smatutor
+{
+public:
+	smapersonnalization( void )
+	{
+		/* place here the actions concerning this library
+		to be realized at the launching of the application  */
+	}
+	~smapersonnalization( void )
+	{
+		/* place here the actions concerning this library
+		to be realized at the ending of the application  */
+	}
+};
+
+
+/*$END$*/
+				  /********************************************/
+				  /* do not modify anything belove this limit */
+				  /*			  unless specified		   	  */
+/******************************************************************************/
+
+// 'static' by GNU C++.
+
+static smapersonnalization Tutor;
+
+ttr_tutor &SMATutor = Tutor;
