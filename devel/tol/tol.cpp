bd0c39417ee98c7574a78df9fac895468dc5842d
epeios-q37 <epeios-q37@users.noreply.github.com>
2002-04-05 10:07:48 +0000

    Misc.

--- devel/tol/tol.cpp
+++ devel/tol/tol.cpp
@@ -1,9 +1,9 @@
 /*
-  'tol' library by Claude L. Simon (simon@epeios.org)
+  'tol' library by Claude SIMON (csimon@epeios.org)
   Requires the 'tol' header file ('tol.h').
-  Copyright (C) 2000,2001 Claude L. SIMON (simon@epeios.org).
+  Copyright (C) 2000-2002 Claude SIMON (csimon@epeios.org).
 
-  This file is part of the Epeios (http://www.epeios.org/) project.
+  This file is part of the Epeios (http://epeios.org/) project.
   
 
   This library is free software; you can redistribute it and/or
--- devel/tol/tol.h
+++ devel/tol/tol.h
@@ -1,8 +1,8 @@
 /*
-  Header for the 'tol' library by Claude L. Simon (simon@epeios.org)
-  Copyright (C) 2000,2001 Claude L. SIMON (simon@epeios.org) 
+  Header for the 'tol' library by Claude SIMON (csimon@epeios.org)
+  Copyright (C) 2000-2002 Claude SIMON (csimon@epeios.org) 
 
-  This file is part of the Epeios (http://www.epeios.org/) project.
+  This file is part of the Epeios (http://epeios.org/) project.
   
 
   This library is free software; you can redistribute it and/or
@@ -32,7 +32,7 @@
 
 #define	TOL_VERSION	"$Revision$"	
 
-#define TOL_OWNER		"the Epeios project (http://www.epeios.org/)"
+#define TOL_OWNER		"Claude SIMON (csimon@epeios.org)"
 
 #include "ttr.h"
 
@@ -45,7 +45,7 @@ extern class ttr_tutor &TOLTutor;
 /* Begin of automatic documentation generation part. */
 
 //V $Revision$
-//C Claude L. SIMON (simon@epeios.org)
+//C Claude SIMON (csimon@epeios.org)
 //R $Date$
 
 /* End of automatic documentation generation part. */
