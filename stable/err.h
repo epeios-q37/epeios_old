5f42a7304bcfb53c830da22fa638ea31ef88081f
epeios-q37 <epeios-q37@users.noreply.github.com>
2002-10-07 10:00:43 +0000

    Some modifications on ERR library.

--- stable/err.h
+++ stable/err.h
@@ -1,25 +1,24 @@
 /*
-  Header for the 'err' library by Claude L. Simon (csimon@webmails.com)
-  Copyright (C) 2000,2001 Claude L. SIMON (csimon@webmails.com) 
+	Header for the 'err' library by Claude SIMON (csimon@epeios.org)
+	Copyright (C) 2000-2002  Claude SIMON (csimon@epeios.org).
 
-  This file is part of the Epeios (http://epeios.org/) project.
-  
+	This file is part of the Epeios (http://epeios.org/) project.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
  
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, go to http://www.fsf.org/
-  or write to the:
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, go to http://www.fsf.org/
+	or write to the:
   
-                        Free Software Foundation, Inc.,
+         	         Free Software Foundation, Inc.,
            59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
@@ -30,22 +29,22 @@
 
 #define ERR_NAME		"ERR"
 
-#define	ERR_VERSION	"$Revision$"	
+#define	ERR_VERSION	"$Revision$"
 
-#define ERR_OWNER		"the Epeios project (http://epeios.org/)"
+#define ERR_OWNER		"Claude SIMON (csimon@epeios.org)"
 
 #include "ttr.h"
 
 extern class ttr_tutor &ERRTutor;
 
 #if defined( XXX_DBG ) && !defined( ERR_NODBG )
-#define ERR_DBG 
+#define ERR_DBG
 #endif
 
 /* Begin of automatic documentation generation part. */
 
 //V $Revision$
-//C Claude L. SIMON (csimon@webmails.com)
+//C Claude SIMON (csimon@epeios.org)
 //R $Date$
 
 /* End of automatic documentation generation part. */
@@ -54,6 +53,11 @@ extern class ttr_tutor &ERRTutor;
 				  /* do not modify anything above this limit */
 				  /*			  unless specified			 */
 				  /*******************************************/
+
+/* Addendum to the automatic documentation generation part. */
+//D ERRor handling 
+/* End addendum to automatic documentation generation part. */
+
 /*$BEGIN$*/
 
 #include <stdio.h>
@@ -96,8 +100,10 @@ namespace err {
 		// prohibibition error; a prohibited fonction was called.
 		lmt = 512,
 		// limit error; attempt to exceed the limit
-		mem = 1024
+		mem = 1024,
 		// memory error (all memory type).
+		ext = 2048,
+		// external error; third part error.
 	};
 	enum a
 	{
@@ -154,6 +160,11 @@ namespace err {
 		mGeneric
 	};
 		// memory error
+	enum x
+	{
+		xGeneric
+	};
+		// external error
 
 #ifdef ERR__THREAD_SAFE
 	// If an error occurs, test if the current thread is concerned.
@@ -236,6 +247,10 @@ namespace err {
 #define ERRM( m )		ERRCommon( err::mem, m )
 //m Throw a memory error.
 #define ERRm()			ERRM( mGeneric )
+
+#define ERRX( m )		ERRCommon( err::ext, m )
+//m Throw a memory error.
+#define ERRx()			ERRX( xGeneric )
 
 #ifdef ERR_JMPUSE
 //m Throw the handler.
