62f46d17d792868cc25b796fb3d735c614bace32
epeios-q37 <epeios-q37@users.noreply.github.com>
2002-10-14 16:45:12 +0000

    Adding library to modify.

--- devel/str/str.h
+++ devel/str/str.h
@@ -1,25 +1,24 @@
 /*
-  Header for the 'str' library by Claude L. Simon (simon@epeios.org)
-  Copyright (C) 2000,2001 Claude L. SIMON (simon@epeios.org) 
+	Header for the 'str' library by Claude SIMON (csimon@epeios.org)
+	Copyright (C) 2000-2002  Claude SIMON (csimon@epeios.org).
 
-  This file is part of the Epeios (http://www.epeios.org/) project.
-  
+	This file is part of the Epeios (http://epeios.org/) project.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
  
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, go to http://www.fsf.org/
-  or write to the:
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, go to http://www.fsf.org/
+	or write to the:
   
-                        Free Software Foundation, Inc.,
+         	         Free Software Foundation, Inc.,
            59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
@@ -30,22 +29,22 @@
 
 #define STR_NAME		"STR"
 
-#define	STR_VERSION	"$Revision$"	
+#define	STR_VERSION	"$Revision$"
 
-#define STR_OWNER		"the Epeios project (http://www.epeios.org/)"
+#define STR_OWNER		"Claude SIMON (csimon@epeios.org)"
 
 #include "ttr.h"
 
 extern class ttr_tutor &STRTutor;
 
 #if defined( XXX_DBG ) && !defined( STR_NODBG )
-#define STR_DBG 
+#define STR_DBG
 #endif
 
 /* Begin of automatic documentation generation part. */
 
 //V $Revision$
-//C Claude L. SIMON (simon@epeios.org)
+//C Claude SIMON (csimon@epeios.org)
 //R $Date$
 
 /* End of automatic documentation generation part. */
@@ -54,6 +53,11 @@ extern class ttr_tutor &STRTutor;
 				  /* do not modify anything above this limit */
 				  /*			  unless specified			 */
 				  /*******************************************/
+
+/* Addendum to the automatic documentation generation part. */
+//D STRing 
+/* End addendum to automatic documentation generation part. */
+
 /*$BEGIN$*/
 
 #include "err.h"
@@ -85,6 +89,16 @@ namespace str {
 		{
 			E_BUNCH_( bso::char__ )::plug( Driver );
 		}
+		void plug( mmm::multimemory_ &M )
+		{
+			E_BUNCH_( bso::char__ )::plug( M );
+		}
+		string_ &operator =( const string_ &O )
+		{
+			E_BUNCH_( bso::char__ )::operator =( O );
+
+			return *this;
+		}
 		//f Initialization.
 		void Init( void )
 		{
@@ -95,15 +109,12 @@ namespace str {
 		{
 			E_BUNCH_( bso::char__ )::Init( Seed, strlen( Seed ) );
 		}
-		string_ &operator =( const string_ &O )
+		//f Initialization with 'Seed'.
+		void Init( const str::string_ &Seed )
 		{
-			E_BUNCH_( bso::char__ )::operator =( O );
+			Init();
 
-			return *this;
-		}
-		void plug( mmm::multimemory_ &M )
-		{
-			E_BUNCH_( bso::char__ )::plug( M );
+			this->operator =( Seed );
 		}
 		string_ &operator =( const char *Chaine )
 		{
