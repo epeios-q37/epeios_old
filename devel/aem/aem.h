135652b642a35ae406dacd82b593e2346a97d4d7
epeios-q37 <epeios-q37@users.noreply.github.com>
2003-01-27 17:38:15 +0000

    Improvements.

--- devel/aem/aem.h
+++ devel/aem/aem.h
@@ -1,25 +1,24 @@
 /*
-  Header for the 'aem' library by Claude SIMON (csimon@epeios.org)
-  Copyright (C) 2002 Claude SIMON (csimon@epeios.org) 
+	Header for the 'aem' library by Claude SIMON (csimon@epeios.org)
+	Copyright (C) 2002-2003  Claude SIMON (csimon@epeios.org).
 
-  This file is part of the Epeios (http://epeios.org/) project.
-  
+	This file is part of the Epeios (http://epeios.org/) project.
 
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
+	This library is free software; you can redistribute it and/or
+	modify it under the terms of the GNU General Public License
+	as published by the Free Software Foundation; either version 2
+	of the License, or (at your option) any later version.
  
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, go to http://www.fsf.org/
-  or write to the:
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, go to http://www.fsf.org/
+	or write to the:
   
-                        Free Software Foundation, Inc.,
+         	         Free Software Foundation, Inc.,
            59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
@@ -30,7 +29,7 @@
 
 #define AEM_NAME		"AEM"
 
-#define	AEM_VERSION	"$Revision$"	
+#define	AEM_VERSION	"$Revision$"
 
 #define AEM_OWNER		"Claude SIMON (csimon@epeios.org)"
 
@@ -39,7 +38,7 @@
 extern class ttr_tutor &AEMTutor;
 
 #if defined( XXX_DBG ) && !defined( AEM_NODBG )
-#define AEM_DBG 
+#define AEM_DBG
 #endif
 
 /* Begin of automatic documentation generation part. */
@@ -54,6 +53,11 @@ extern class ttr_tutor &AEMTutor;
 				  /* do not modify anything above this limit */
 				  /*			  unless specified			 */
 				  /*******************************************/
+
+/* Addendum to the automatic documentation generation part. */
+//D Amount/Extent managing 
+/* End addendum to automatic documentation generation part. */
+
 /*$BEGIN$*/
 
 /* Addendum to the automatic documentation generation part. */
@@ -283,6 +287,11 @@ namespace aem {
 		bso::bool__ IsEmpty( void ) const
 		{
 			return Amount() == 0;
+		}
+		//f Return true if 'Row' exists, false otherwise.
+		bso::bool__ Exists( row Row )
+		{
+			return *Row < Amount();
 		}
 	};
 
