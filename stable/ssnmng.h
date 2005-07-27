17b7ad517eea47c66037c4d4a6209545403bfeae
epeios-q37 <epeios-q37@users.noreply.github.com>
2005-07-27 14:50:57 +0000

    Codage.

--- stable/ssnmng.h
+++ stable/ssnmng.h
@@ -246,9 +246,9 @@ namespace ssnmng {
 			if ( time( &C.Relative ) == -1 )
 				ERRs();
 
-			Chronos.Store( C, P );
-
 			C.AlwaysValid = false;
+
+			Chronos.Store( C, P );
 
 	#ifdef SSNMNG_DBG
 			if ( _queue_::Amount() == 0 )
