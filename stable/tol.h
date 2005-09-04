b5f2895a268c5c844a909cd256d9a47c5edddfa5
epeios-q37 <epeios-q37@users.noreply.github.com>
2005-09-04 06:57:32 +0000

    Codage.

--- stable/tol.h
+++ stable/tol.h
@@ -873,6 +873,15 @@ namespace tol {
 
 }
 
+// Inspiré du site mscn.microsoft.com.
+#define __STR2__(x) #x
+#define __STR1__(x) __STR2__(x)
+#define __LOC__ __FILE__ "("__STR1__(__LINE__)") : "
+
+// Utilisation :
+// #pragma message(__LOC__"Message")
+
+
 /*$END$*/
 				  /********************************************/
 				  /* do not modify anything belove this limit */
