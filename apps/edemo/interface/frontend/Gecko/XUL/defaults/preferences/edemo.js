af9ed4de7db720769cfcf3213d4689997f35d1cd
epeios-q37 <epeios-q37@users.noreply.github.com>
2012-01-27 17:16:37 +0000

    Codage.

---
+++ apps/edemo/interface/frontend/Gecko/XUL/defaults/preferences/edemo.js
@@ -0,0 +1,8 @@
+// $Id$
+pref("toolkit.defaultChromeURI", "chrome://edemo/content/EDemo.xul");
+pref("browser.formfill.enable", true );
+pref("UniversalBrowserWrite", true );
+
+// Following both lines permit to change the locale.
+pref("intl.locale.matchOS", "false");
+pref("general.useragent.locale", "fr");
