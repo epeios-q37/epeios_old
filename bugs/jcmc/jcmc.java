053531344b614bbfc1ecfb118468291cfeae30e4
epeios-q37 <epeios-q37@users.noreply.github.com>
2012-07-10 15:52:53 +0000

    Codage.

--- bugs/jcmc/jcmc.java
+++ bugs/jcmc/jcmc.java
@@ -3,7 +3,13 @@ class jcmc {
 
 	static public void main ( String[] args )
 	{
+		System.out.print( "Loading library 'jcmc'..." );
+		System.out.flush();
 		System.loadLibrary("jcmc");
+		System.out.println( " 'jcmc' library loaded !" );
+		System.out.print( "Trying to call 'malloc' function from a native component..." );
+		System.out.flush();
 		bug();
+		System.out.println( " 'malloc' calling succeed !" );
 	}
 }
