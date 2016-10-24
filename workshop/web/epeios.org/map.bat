b0918b312ad0da7d4e9eee04c7f2a83ae56408de
epeios-q37 <epeios-q37@users.noreply.github.com>
2016-10-24 16:09:26 +0200

    To void doublons

--- workshop/web/epeios.org/map.bat
+++
@@ -1,5 +0,0 @@
-sabcmd file://t:/map.xsl site.xml i/map.xml
-reveal t:/windows.crp site.crp i/site.crp
-reveal i/site.crp site.xml i/site.xml
-sabcmd file://t:/epeios.org/page.xsl i/site.xml /website//map.html "$hierarchy=" "$name=map"
-start \website\map.html
--- workshop/web/epeios.org/transfert.bat
+++
@@ -1,3 +0,0 @@
-REM $Id$
-
-rsync -v -c -r -e ssh --delete --force --exclude stats/ /cygdrive/h/web/ apache@home.epeios.org:/var/www/www
